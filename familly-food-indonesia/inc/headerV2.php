6b1b11792dda701f6c6f7491c0c3fe56f2798800
aldi.arpateam <aldi1900018037@webmail.uad.ac.id>
2022-10-30 04:58:14 +0700

    1 - 8 November 2022 (Membuat Menu & Header Website)

---
+++ familly-food-indonesia/inc/headerV2.php
@@ -0,0 +1,48 @@
+<header class="container">
+    <div class="row justify-content-center">
+        <div class="col-12 col-sm-10 col-md-5 col-xl-4 text-center">
+            <img src="../assets/images/<?= $logoVersiDesktop ?>" alt="Logo <?= $namaweb; ?>" class="img-fluid">
+        </div>
+    </div>
+</header>
+
+<nav class="navbar sticky-top navbar-expand-md navbar-dark bg-success-navbar-dark shadow-sm py-0 py-md-3">
+    <div class="container">
+        <a class="navbar-brand d-block d-md-none" href="/">
+          <img src="../assets/images/<?= $logoVersiMobile ?>" alt="Logo <?= $namaweb; ?>" height="50">
+        </a>
+
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+        	<hr class="border-danger d-block d-md-none">
+            <ul class="navbar-nav mx-auto">
+                <li class="nav-item">
+                    <a class="nav-link p-2 p-md-2 mt-2 mt-md-0" aria-current="page" href="../">Beranda</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link p-2 p-md-2 mt-2 mt-md-0 <?php if ($_GET['module']==='promo') { echo 'active'; } ?>" href="../promo/">Promo <i class="fas fa-tag"></i></a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link p-2 p-md-2 mt-2 mt-md-0" href="../testimoni">Testimoni</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link p-2 p-md-2 mt-2 mt-md-0" href="../tanya-jawab">Tanya Jawab</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link p-2 p-md-2 mt-2 mt-md-0 <?php if ($_GET['module']==='blog' OR $_GET['module']==='read-blog') { echo 'active'; } ?>" href="../blog/">Blog</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link p-2 p-md-2 mt-2 mt-md-0" href="../keranjang-saya">
+                        <span class="d-inline d-md-none">Keranjang Saya </span>
+                        <i class="fas fa-shopping-cart position-relative pe-3">
+                            <span class="position-absolute top-0 start-100 translate-middle badge rounded-pill bg-danger"><?= $_SESSION['qty_FamillyFood'] ?></span>
+                        </i>
+                    </a>
+                </li>
+            </ul>
+        </div>
+    </div>
+</nav>
